633ee92f12bfd8d2b5b9e5bdeed47d89ff54c46a
mjcumming <mike@4831.com>
2019-05-11 14:50:34 -0500

    version update for pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 
 setuptools.setup(name='Homie3',
-      version='0.0.9',
+      version='0.1.0',
       description='Homie 3.0.1 Implementation',
       author='Michael Cumming',
       author_email='mike@4831.com',
